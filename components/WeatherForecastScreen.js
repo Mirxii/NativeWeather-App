69e84fcbac3d13053d617a15ae8d3b6d98d79bfa
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-01-22 21:55:11 +0200

    Added Forecast view

--- components/WeatherForecastScreen.js
+++ components/WeatherForecastScreen.js
@@ -1,10 +1,51 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { OPEN_WEATHER_API_KEY } from "@env";
+import { FlatList } from "react-native";
+
+import CityInput from "./CityInput";
+import WeatherListItem from "./WeatherListItem";
 
 const WeatherForecastScreen = () => {
+  const [city, setCity] = useState("Tampere");
+  const [forecastData, setForecastData] = useState({});
+
+  const fetchForecastData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${OPEN_WEATHER_API_KEY}`
+      );
+      const data = await response.json();
+      setForecastData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const cityHandler = (city) => {
+    setCity(city);
+  };
+
+  let cityTitle = forecastData.city ? forecastData.city.name : "Lookup a city";
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>WeatherForecastScreen</Text>
+      <Text style={styles.text}>{cityTitle}</Text>
+      <FlatList
+        style={styles.list}
+        data={forecastData.list}
+        renderItem={({ item }) => (
+          <WeatherListItem
+            time={item.dt_txt}
+            description={item.weather[0].main}
+            temperature={item.main.temp}
+            wind={item.wind.speed}
+          />
+        )}
+        keyExtractor={(item) => item.dt_txt}
+      />
+      <CityInput handleCityChange={cityHandler} />
+      <Button title="Get Forecast" onPress={() => fetchForecastData()} />
     </View>
   );
 };
@@ -13,6 +54,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "antiquewhite",
+    padding: 8,
+  },
+  list: {
+    flex: 1,
     padding: 8,
   },
   text: {
--- components/WeatherListItem.js
+++ components/WeatherListItem.js
@@ -1,10 +1,79 @@
-const WeatherListItem = ({weatherData}) => {
+import React, { useMemo, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const WeatherListItem = ({ time, description, temperature, wind }) => {
+  [icon, setIcon] = useState("");
+  [hours, setHours] = useState("");
+
+  const getIcon = () => {
+    switch (description) {
+      case "Clear":
+        setIcon("weather-sunny");
+        break;
+      case "Clouds":
+        setIcon("weather-cloudy");
+        break;
+      case "Rain":
+        setIcon("weather-pouring");
+        break;
+      case "Drizzle":
+        setIcon("weather-rainy");
+        break;
+      case "Thunderstorm":
+        setIcon("weather-lightning");
+        break;
+      case "Snow":
+        setIcon("weather-snowy");
+        break;
+      case "Mist":
+        setIcon("weather-fog");
+        break;
+      default:
+        setIcon("weather-fog");
+    }
+  };
+
+  const getHours = () => {
+    let parsedHours = `${time.split(" ")[1].split(":")[0]}`;
+    setHours(parsedHours);
+  };
+
+  useMemo(() => {
+    getIcon();
+  }, [description]);
+
+  useMemo(() => {
+    getHours();
+  }, [time]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{weatherData.city}</Text>
-      <Text style={styles.text}>{weatherData.temperature}</Text>
-      <Text style={styles.text}>{weatherData.windSpeed}</Text>
-      <Text style={styles.text}>{weatherData.description}</Text>
+      <Text style={styles.text}>{hours}</Text>
+      <MaterialCommunityIcons name={icon} size={22} color="darkslategray" />
+      <Text style={styles.text}>{temperature}</Text>
+      <Text style={styles.text}>{wind}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    backgroundColor: "antiquewhite",
+    padding: 8,
+    borderBottomColor: "darkslategray",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  text: {
+    fontSize: 22,
+    color: "darkslategray",
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontWeight: "bold",
+  },
+});
+
+export default WeatherListItem;
